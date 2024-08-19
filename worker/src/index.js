591b6b58c7e09958b5c4a0d3e96870e23b5ff5d1
Virajb19 <virajb853@gmail.com>
2024-08-19 23:05:54 +0530

    v3

--- worker/src/index.js
+++ worker/src/index.js
@@ -10,6 +10,10 @@ async function main() {
   await consumer.connect();
   await consumer.subscribe({ topic: "zap-events", fromBeginning: true });
 
+  const producer = kafka.producer()
+
+  await producer.connect( )
+
   await consumer.run({
     autoCommit: false,
     eachMessage: async ({ topic, partition, message }) => {
@@ -25,10 +29,42 @@ async function main() {
 
       const zapRunDetails = await client.zapRun.findFirst({
         where: {id: zapRunId},
-        include: {zap: {include: {trigger: true}}}
+        include: {zap: {include: {actions: {include: {type: true}}}}}
       })
 
+      const currentAction = zapRunDetails?.zap.actions.find(x => x.sortingOrder === stage)
+
+      if(!currentAction){
+        console.log("Current action not found")
+        return
+      }
+
+      console.log(currentAction)
+
+      if(currentAction.type.name === "email"){
+         console.log("sending out an email");   
+      }
+
+      if(currentAction.type.name === "solana"){
+        console.log("sending out a solana")
+      }
+
       await new Promise(r => setTimeout(r, 500))
+
+      const zapId = message.value?.toString()
+      const laststage = (zapRunDetails?.zap.actions?.length || 1) - 1
+
+      if(laststage != stage){
+         await producer.send({
+          topic: 'zap-events',
+          messages: [{
+            value: JSON.stringify({
+              stage: stage + 1,
+              zapRunId
+            })
+          }]
+         })
+      }
 
       console.log('processing done')
 
